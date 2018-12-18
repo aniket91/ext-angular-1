c2752d2c4151b2d570e66ea9405be3dcf6cea5bd
alisha <alisha.agarwal@joshtechnologygroup.com>
2018-12-18 17:57:33 +0530

    Added doc for ChipView.Updated colorpicker exmples.

--- packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
+++ packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
@@ -2,35 +2,20 @@
             import { Component } from '@angular/core'
             declare var Ext: any;
 
-            Ext.require('Ext.ux.gauge.needle.Arrow');
-
             @Component({
                 selector: 'app-root-1',
                 styles: [`
                         `],
                 template: `
-                    <formpanel #item shadow="true" layout="vbox" maxWidth="850">
-                        <sliderfield #item label="Value" width="350" (change)="updateGauges($event)" [value]="needleValue">
-                        </sliderfield>
-                        <container #item
-                            [layout]="{type: 'hbox', align: 'stretch'}"
-                            [margin]="'10 0 10 0'"
-                            flex="1"
-                            [width]="'100%'"
-                            minHeight="200"
-                        >
-                             <gauge #item flex="1" [value]="needleValue" 
-                                [needle]="{outerRadius: '100%'}"
-                                [valueStyle]="{display: 'none'}
-                            ></gauge>
-                        </container>
-                    </formpanel>
+                    <colorselector #item
+                        (change)="this.onChange($event)"
+                        format="hex6" 
+                        value="00f"
+                    ></colorselector>
                 `
             })
             export class AppComponent {
-                needleValue:number = 30;
-                
-                updateGauges = (param) => {
-                    this.needleValue = param.newValue;
+                onChange = (param) => {
+                    Ext.Msg.alert('Color', param.color);
                 }
             }
